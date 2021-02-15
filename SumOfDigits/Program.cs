b4577b1bf0de03f9bbbbf4cdd87c3e578c23620c
Kushal Neupane <mkushal10@gmail.com>
2021-02-15 20:20:11 +0545

    sum of digits project commit

---
+++ SumOfDigits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number : ");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\n");
+
+            if (n1!=0 && n2!=0)
+            {
+                int total = n1 + n2;
+                Console.WriteLine("Total of two numbers is " + total);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Write("You entered both 0 value.");
+                Console.Read();
+            }
+        }
+    }
+}
